3216cdf3d39429e39ef667670f52da2a2e41c1f1
Yasser Alghunaim <alghunaimyasser@gmail.com>
2019-03-12 00:52:23 +0300

    homework

--- starter-code/madlib-with-loops/js/app.js
+++ starter-code/madlib-with-loops/js/app.js
@@ -1,7 +1,25 @@
 var startupX = ['Uber', 'Google', 'Amazon', 'Apple', 'Facebook', 'Twitter'];
 var startupY = ['Slack', 'Trello', 'Tesla', 'Hyperloop', 'Harvest'];
+var fav =[];
 
-var random1 = Math.floor((Math.random() * startupX.length));
-var random2 = Math.floor((Math.random() * startupY.length));
+
+function getRandom () {
+    var random1 = Math.floor((Math.random() * startupX.length));
+    var random2 = Math.floor((Math.random() * startupY.length));
+    var _text = 'A startup that is ' + startupX[random1] + ', but for ' + startupY[random2];
+   document.getElementById("xForY").innerHTML= _text;
+  }
+
+  function saveStartup () {
+    fav.push(document.getElementById("xForY").innerHTML);
+    }
+
+    function showfav () {
+      document.getElementById("favorites").innerHTML= fav.join("<br />");
+    }
+
+  document.getElementById("create").onclick = getRandom;
+  document.getElementById("save").onclick = saveStartup;
+  document.getElementById("print").onclick = showfav;
 
 console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
